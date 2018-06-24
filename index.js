e9cb6836460af16bd95a9c7d26df8c9d0c2f27f7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-06-24 12:01:58 +0200

    ok

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ module.exports = (a) => {
     const { execute, log } = a;
     return {
         image: {
-            list: 'docker images',
+            list: 'docker images [...options]',
             delete: 'docker image rm <image>',
         },
         container: {
             list: 'docker container ls --all',
+            empty: 'docker container prune --force',
+            delete: 'docker container rm <container>',
         }
     };
 };
